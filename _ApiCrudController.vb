78dcb2b3e6d65436e0361da25fc20e8eac3c4328
rguerra061080 <renatomguerra@ig.com.br>
2016-11-17 19:17:14 -0200

    Add files via upload

---
+++ _ApiCrudController.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Net
+Imports System.Net.Http
+Imports System.Web.Http
+Imports System.Reflection
+Imports CadastroMedicos.UI.Common
+Imports CadastroMedicos.Domain
+Imports CadastroMedicos.Data
+Imports CadastroMedicos.Data.Repositories.Generic
+
+Namespace Controllers.api
+
+    Public MustInherit Class ApiCrudController(Of TDtoObject As DataTransferObject, TDataEntity As Class)
+        Inherits ApiController
+
+        Protected unitOfWork As IUnitOfWork
+
+        Public Sub New()
+            unitOfWork = New UnitOfWork()
+        End Sub
+
+        Public Sub New(unitOfWork As IUnitOfWork)
+            MyClass.unitOfWork = unitOfWork
+        End Sub
+
+        Protected Function CreateDtoInstance(dataEntity As TDataEntity) As TDtoObject
+            Dim dto As TDtoObject
+
+            dto = Activator.CreateInstance(GetType(TDtoObject)) 'New TDtoObject
+
+            Utils.TransferDataFromObject(dataEntity, dto)
+            Return dto
+        End Function
+
+        Protected MustOverride Function GetRepository() As IRepository(Of TDataEntity)
+
+        <HttpGet>
+        Public Function GetAll() As IHttpActionResult
+
+            Dim repository As IRepository(Of TDataEntity) = GetRepository()
+
+            Dim resultType As Func(Of TDataEntity, TDtoObject)
+            Dim allData As IList(Of TDataEntity)
+            Dim dtoResult As IList(Of TDtoObject)
+
+            allData = repository.GetAll().ToList()
+
+            resultType = Function(dataEntity As TDataEntity)
+                             Return CreateDtoInstance(dataEntity)
+                         End Function
+
+            dtoResult = allData.Select(resultType).ToList()
+            Return Ok(dtoResult)
+        End Function
+
+        <HttpGet>
+        Public Function GetById(id As Long) As TDtoObject
+
+            Dim dto As TDtoObject
+            Dim dataEntity As TDataEntity
+
+            dto = Activator.CreateInstance(GetType(TDtoObject)) 'New TDtoObject
+
+            dataEntity = GetRepository().GetById(id)
+            If Not IsNothing(dataEntity) Then
+                Utils.TransferDataFromObject(dataEntity, dto)
+            End If
+
+            Return dto
+        End Function
+
+        <HttpPost>
+        Public Function Post(dto As TDtoObject) As IHttpActionResult
+
+            Dim dataEntity As TDataEntity = Activator.CreateInstance(GetType(TDataEntity))
+            Dim repository As IRepository(Of TDataEntity) = GetRepository()
+            Try
+                Utils.TransferDataFromObject(dto, dataEntity)
+                repository.Insert(dataEntity)
+                unitOfWork.SaveChanges()
+                Return Ok("Registro incluído com sucesso.")
+            Catch ex As Exception
+                Return BadRequest(String.Format("Erro ao processar a solicitação: {0}", ex.Message))
+            End Try
+
+        End Function
+
+        <HttpPut>
+        Public Function Put(id As Integer, dto As TDtoObject) As IHttpActionResult
+
+            Dim dataEntity As TDataEntity
+            Dim repository As IRepository(Of TDataEntity)
+
+            Try
+                repository = GetRepository()
+                dataEntity = repository.GetById(dto.Id)
+
+                If dataEntity Is Nothing Then
+                    Return BadRequest("Registro não encontrado")
+                End If
+
+                Utils.TransferDataFromObject(dto, dataEntity)
+                repository.Update(dataEntity)
+
+                unitOfWork.SaveChanges()
+
+                Return Ok("Registro atualizado com sucesso.")
+            Catch ex As Exception
+                Return BadRequest(String.Format("Erro ao processar a solicitação: {0}", ex.Message))
+            End Try
+        End Function
+
+
+        <HttpDelete>
+        Public Function Delete(id As Integer) As IHttpActionResult
+
+            Dim dataEntity As TDataEntity
+            Dim repository As IRepository(Of TDataEntity)
+
+            Try
+                repository = GetRepository()
+                dataEntity = repository.GetById(id)
+
+                If dataEntity Is Nothing Then
+                    Return BadRequest("Registro não encontrado")
+                End If
+
+                repository.Delete(dataEntity)
+                unitOfWork.SaveChanges()
+
+                Return Ok("Registro excluído com sucesso.")
+            Catch ex As Exception
+                Return BadRequest(String.Format("Erro ao processar a solicitação: {0}", ex.Message))
+            End Try
+
+        End Function
+
+    End Class
+
+End Namespace
---
+++ _CrudController.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Web.Mvc
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Net.Http.Formatting
+Imports System.Configuration
+Imports System.Threading.Tasks
+Imports Newtonsoft.Json
+Imports CadastroMedicos.Domain
+
+Namespace Controllers
+    Public Class CrudController(Of TDtoObject As DataTransferObject)
+        Inherits Controller
+
+        Private _controllerName As String
+
+        Public Sub New(controllerName As String)
+            _controllerName = controllerName
+        End Sub
+
+        <HttpGet>
+        Public Async Function Index() As Task(Of ActionResult)
+            Dim dtoResult As IList(Of TDtoObject)
+
+            Using client As New HttpClient()
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                Using response As HttpResponseMessage = Await client.GetAsync(_controllerName)
+                    If response.IsSuccessStatusCode Then
+                        Dim jsonResult As String
+                        jsonResult = Await response.Content.ReadAsStringAsync()
+                        dtoResult = JsonConvert.DeserializeObject(Of TDtoObject())(jsonResult).ToList
+                    End If
+                End Using
+            End Using
+
+            Return View(dtoResult)
+        End Function
+
+        <HttpGet>
+        Public Function Create() As ActionResult
+            Return View()
+        End Function
+
+        <HttpPost>
+        Public Async Function Create(dto As TDtoObject) As Task(Of ActionResult)
+            Dim uri As String
+            Dim jsonResult As String
+            Dim content As StringContent
+
+            uri = String.Format("{0}",_controllerName)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                jsonResult = JsonConvert.SerializeObject(dto)
+                content = New StringContent(jsonResult, Encoding.UTF8, "application/json")
+
+                Using response As HttpResponseMessage = Await client.PostAsync(uri, content)
+                    If response.IsSuccessStatusCode Then
+                        Return RedirectToAction("Index")
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                        Return View(dto)
+                    End If
+                End Using
+            End Using
+
+        End Function
+
+        <HttpGet>
+        Public Async Function Edit(id As Long) As Task(Of ActionResult)
+            Dim dtoResult As TDtoObject
+            Dim uri As String
+
+            uri = String.Format("{0}/{1}", _controllerName, id)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                Using response As HttpResponseMessage = Await client.GetAsync(uri)
+                    If response.IsSuccessStatusCode Then
+                        Dim jsonResult As String
+                        jsonResult = Await response.Content.ReadAsStringAsync()
+                        dtoResult = JsonConvert.DeserializeObject(Of TDtoObject)(jsonResult)
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                    End If
+                End Using
+            End Using
+
+            Return View(dtoResult)
+        End Function
+
+        <HttpPost>
+        Public Async Function Edit(dto As TDtoObject) As Task(Of ActionResult)
+
+            Dim uri As String
+            Dim jsonResult As String
+            Dim content As StringContent
+
+            uri = String.Format("{0}/{1}", _controllerName, dto.Id)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                jsonResult = JsonConvert.SerializeObject(dto)
+                content = New StringContent(jsonResult, Encoding.UTF8, "application/json")
+
+                Using response As HttpResponseMessage = Await client.PutAsync(uri, content)
+                    If response.IsSuccessStatusCode Then
+                        Return RedirectToAction("Index")
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                        Return View(dto)
+                    End If
+                End Using
+            End Using
+        End Function
+
+
+        <HttpGet>
+        Public Async Function Details(id As Long) As Task(Of ActionResult)
+
+            Dim dtoResult As TDtoObject
+            Dim uri As String
+
+            uri = String.Format("{0}/{1}", _controllerName, id)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                Using response As HttpResponseMessage = Await client.GetAsync(uri)
+                    If response.IsSuccessStatusCode Then
+                        Dim jsonResult As String
+                        jsonResult = Await response.Content.ReadAsStringAsync()
+                        dtoResult = JsonConvert.DeserializeObject(Of TDtoObject)(jsonResult)
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                    End If
+                End Using
+            End Using
+
+            Return View(dtoResult)
+        End Function
+
+        <HttpGet>
+        Public Async Function Delete(id As Long) As Task(Of ActionResult)
+            Dim dtoResult As TDtoObject
+            Dim uri As String
+
+            uri = String.Format("{0}/{1}", _controllerName, id)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                Using response As HttpResponseMessage = Await client.GetAsync(uri)
+                    If response.IsSuccessStatusCode Then
+                        Dim jsonResult As String
+                        jsonResult = Await response.Content.ReadAsStringAsync()
+                        dtoResult = JsonConvert.DeserializeObject(Of TDtoObject)(jsonResult)
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                    End If
+                End Using
+            End Using
+
+            Return View(dtoResult)
+        End Function
+
+        <HttpPost>
+        <ActionName("Delete")>
+        Public Async Function ConfirmDelete(dto As TDtoObject) As Task(Of ActionResult)
+
+            Dim uri As String
+            Dim jsonResult As String
+            Dim content As StringContent
+
+            uri = String.Format("{0}/{1}", _controllerName, dto.Id)
+
+            Using client As New HttpClient
+                client.BaseAddress = New Uri(ConfigurationManager.AppSettings("ServiceURI"))
+                jsonResult = JsonConvert.SerializeObject(dto)
+                content = New StringContent(jsonResult, Encoding.UTF8, "application/json")
+
+                Using response As HttpResponseMessage = Await client.DeleteAsync(uri)
+                    If response.IsSuccessStatusCode Then
+                        Return RedirectToAction("Index")
+                    Else
+                        ModelState.AddModelError(String.Empty, "Erro ao processar solicitar a requisição")
+                        Return View(dto)
+                    End If
+                End Using
+            End Using
+
+
+        End Function
+
+    End Class
+End Namespace
